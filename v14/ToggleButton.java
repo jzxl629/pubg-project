02ce10758b17ce1587a5f3507a6ae6b47b0e3658
Zhouqian Lu <40674707+jzxl629@users.noreply.github.com>
2018-11-21 14:14:12 -0500

    Add files via upload
    TestGame added

---
+++ v14/ToggleButton.java
@@ -0,0 +1,9 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ToggleButton extends javafx.scene.control.Button {
+    boolean toggle;
+    public ToggleButton(){
+        toggle = false;
+    }
+}
---
+++ v14/main.java
@@ -0,0 +1,7 @@
+public class main  {
+    //Runs the GUI
+    public static void main(String args[]){
+        GUI g = new GUI();
+        g.run(args);
+    }
+}
